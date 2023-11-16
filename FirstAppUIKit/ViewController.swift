f8acbf613f6687a0bf2c3c36d8665187a9c37cba
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-11-17 05:46:52 +0700

    added logoImage

--- FirstAppUIKit/ViewController.swift
+++ FirstAppUIKit/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    let logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .yellow
+        imageView.image = UIImage(named: "NewImage")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    
+    
+    
+ 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        
+        setupImage()
+        setupConstraints()
+        }
+
+    private func setupImage(){
+        view.addSubview(logoImage)
     }
-
-
+    
+    func setupConstraints(){
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // MARK: logoImage
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            logoImage.heightAnchor.constraint(equalToConstant: view.frame.size.height / 4)
+            
+            
+        ])
+    }
 }
-
+#Preview {
+    ViewController()
+}
